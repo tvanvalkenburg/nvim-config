42d61c22914021b21585dcb6c48ddfef26acf135
Taylor van Valkenburg <taylor.vanvalkenburg@lingarogroup.com>
2026-04-24 12:42:58 +0200

    initial commit

---
+++ lua/custom/plugins/lazygit.lua
@@ -0,0 +1,19 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  'kdheepak/lazygit.nvim',
+  lazy = true,
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  keys = {
+    { '<leader>gg', '<cmd>LazyGit<cr>', desc = '[G]it: Lazy[g]it' },
+  },
+}
